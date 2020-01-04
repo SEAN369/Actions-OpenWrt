d403255ca3ea479179161c300f1a06577dd26569
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-01-04 19:02:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,6 @@ git clone https://github.com/pymumu/luci-app-smartdns
 git clone https://github.com/LGA1150/openwrt-fullconenat
 git clone https://github.com/rufengsuixing/luci-app-onliner
 git clone https://github.com/lisaac/luci-app-diskman
-https://github.com/lisaac/luci-app-diskman
 # git clone https://github.com/mchome/luci-app-vlmcsd
 git clone https://github.com/mchome/openwrt-vlmcsd vlmcsd
 git clone https://github.com/KFERMercer/openwrt-v2ray v2ray
