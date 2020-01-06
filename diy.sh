a7ff4b86fdb8d39d3eed3ca1c8290ff9e0c5ffff
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-01-06 09:23:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,9 +2,10 @@
 #=================================================
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean
 cd package/lean
-git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic
+
 cd -
 cd package/feeds
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic
 git clone https://github.com/rufengsuixing/luci-app-adguardhome
 git clone https://github.com/jerrykuku/luci-theme-argon
 git clone https://github.com/pymumu/luci-app-smartdns
