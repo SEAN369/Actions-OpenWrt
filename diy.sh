2242032e69e3e30c6d77143525249d5980f0074b
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-01-08 19:39:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -40,6 +40,10 @@ sed -i '$a /etc/smartdns' package/base-files/files/lib/upgrade/keep.d/base-files
 sed -i "s/option bbr '0'/option bbr '1'/g" package/*/luci-app-flowoffload/root/etc/config/flowoffload
 find target/linux/x86 -name "config*" | xargs -i sed -i '$a # CONFIG_WLAN is not set' {}
 find target/linux/x86 -name "config*" | xargs -i sed -i '$a # CONFIG_WIRELESS is not set' {}
+find target/linux/x86 -name "config*" | xargs -i sed -i '$i CONFIG_HWMON=y' {}
+find target/linux/x86 -name "config*" | xargs -i sed -i '$i CONFIG_SENSORS_CORETEMP=y' {}
+find target/linux/x86 -name "config*" | xargs -i sed -i '$i CONFIG_GPIOLIB=y' {}
+find target/linux/x86 -name "config*" | xargs -i sed -i '$i CONFIG_I2C=y' {}
 #sed -i 's/fast_open="0"/fast_open="1"/g' package/*/luci-app-passwall/root/usr/share/passwall/subscription.sh
 sed -i '/switch_enable/d' package/*/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.sh
 sed -i 's/fast_open="0"/fast_open="1"/g' package/*/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.sh
