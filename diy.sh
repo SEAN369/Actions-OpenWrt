de8d7cec780cdd708ac5a4f1c9866beddbe8e165
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-01-13 22:16:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -15,7 +15,7 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome
 git clone https://github.com/jerrykuku/luci-theme-argon
 git clone https://github.com/pymumu/luci-app-smartdns
 git clone https://github.com/LGA1150/openwrt-fullconenat
-git clone -b openwrt-19 https://github.com/rufengsuixing/luci-app-onliner
+git clone https://github.com/rufengsuixing/luci-app-onliner
 git clone https://github.com/lisaac/luci-app-diskman
 mkdir parted && cp luci-app-diskman/Parted.Makefile parted/Makefile
 # git clone https://github.com/mchome/luci-app-vlmcsd
