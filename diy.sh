1e4c1690f215f480ab4d9d818fbdd48f743c9443
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-02-03 22:53:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -57,6 +57,7 @@ sed -i 's/test_proxy()/test_proxy1()/g' package/lean/luci-app-ssr-plus/root/usr/
 sed -i '$a /etc/smartdns' package/base-files/files/lib/upgrade/keep.d/base-files-essential
 sed -i '$a /usr/share/aria2' package/base-files/files/lib/upgrade/keep.d/base-files-essential
 sed -i '$a /etc/nginx' package/base-files/files/lib/upgrade/keep.d/base-files-essential
+sed -i '$a /etc/nginx/nginx.conf' package/base-files/files/lib/upgrade/keep.d/base-files-essential
 sed -i '$a /www/nas/data' package/base-files/files/lib/upgrade/keep.d/base-files-essential
 sed -i '$a /www/nas/plugins' package/base-files/files/lib/upgrade/keep.d/base-files-essential
 find target/linux/x86 -name "config*" | xargs -i sed -i '$a # CONFIG_WLAN is not set\n# CONFIG_WIRELESS is not set\nCONFIG_NETFILTER_XT_MATCH_STRING=m' {}
