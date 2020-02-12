c726be0996be865d3fcacfae94b93471aa907387
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-02-12 17:35:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -84,7 +84,7 @@ basename $(curl -Ls -o /dev/null -w %{url_effective} https://github.com/$1/$2/re
 }
 sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(getversion v2ray v2ray-core)/g" package/lean/v2ray/Makefile
 sed -i "s/PKG_HASH:=.*/PKG_HASH:=skip/g" package/lean/v2ray/Makefile
-sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(getversion aria2 aria2)/g" package/lean/v2ray/Makefile
+sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(getversion aria2 aria2)/g" package/feeds/*/aria2/Makefile
 sed -i "s/PKG_HASH:=.*/PKG_HASH:=skip/g" package/feeds/*/aria2/Makefile
 sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(getversion nondanee UnblockNeteaseMusic)/g" package/*/UnblockNeteaseMusic/Makefile
 sed -i 's/PACKAGE_libcap:libcap/libcap/g' feeds/packages/net/samba4/Makefile
