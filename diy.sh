3e74ac0a1ea838e1c8fe5574bbac45533870462d
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-02-16 21:39:26 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,6 +2,7 @@
 #=================================================
 #sudo npm install -g github-files-fetcher && fetcher --url="https://github.com/openwrt/packages/tree/openwrt-18.06/net/miniupnpd" --out=package/feeds/
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean
+rm -Rf package/lean/luci-app-ssr-plus
 cp -Rf ../diy/* ./
 rm -Rf package/lean/qBittorrent/patches
 sed -i 's/PKG_SOURCE_URL:=.*/PKG_SOURCE_URL:=https:\/\/github.com\/c0re100\/qBittorrent-Enhanced-Edition/g' package/lean/qBittorrent/Makefile
