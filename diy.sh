396de56f74e95135a73a0d85f71507fa2cd662fe
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-02-28 18:51:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -58,6 +58,7 @@ sed -i 's/extra_settings/extra_setting/g' package/feeds/*/aria2/files/aria2.init
 sed -i '$a /etc/smartdns' package/base-files/files/lib/upgrade/keep.d/base-files-essential
 sed -i '$a /www/nas/data' package/base-files/files/lib/upgrade/keep.d/base-files-essential
 sed -i '$a /www/nas/plugins' package/base-files/files/lib/upgrade/keep.d/base-files-essential
+sed -i '$a /www/nas/config/define.php' package/base-files/files/lib/upgrade/keep.d/base-files-essential
 find target/linux/x86 -name "config*" | xargs -i sed -i '$a # CONFIG_WLAN is not set\n# CONFIG_WIRELESS is not set\nCONFIG_NETFILTER_XT_MATCH_STRING=m' {}
 #sed -i 's/fast_open="0"/fast_open="1"/g' package/*/luci-app-passwall/root/usr/share/passwall/subscription.sh
 sed -i 's/if test_proxy/sleep 3600\nif test_proxy/g' package/*/luci-app-vssr/root/usr/bin/vssr-switch
